223e15ae1a410f1616cc134d585381b0bb11ac71
poignant-minifier-0983 <davidfano@gmail.com>
2019-06-10 00:50:33 +0000

    Automatically backed up by Learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,17 +1,13 @@
 class Fish
-  attr_reader :name
-  attr_accessor :mood, :owner
+  attr_accessor :name
 
   @@all = []
 
-  def initialize(name, owner)
-      @name = name
-      @mood = "nervous"
-      @owner = owner
-      @@all << self
+  def initialize(name)
+    @name = name
   end
 
   def self.all
-      @@all
+    @@all
   end
 end
