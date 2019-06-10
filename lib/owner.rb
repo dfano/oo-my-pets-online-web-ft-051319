f173984a3effea8904f8dc4c5745bd0c387c707d
poignant-minifier-0983 <davidfano@gmail.com>
2019-06-10 00:28:21 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,12 @@
 class Owner
-  attr_accessor :name
+  attr_accessor :name, :species
 
   @@all = []
 
   def initialize(name)
     @name = name
     @@all << self
+    @species = species
   end
 
   def self.all
